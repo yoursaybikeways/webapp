e7115d5b729dae703330bf88aede733f156e61bc
Tom Watson <tom.watson@kinesis.org>
2022-09-22 21:36:56 +1000

    basic map layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Pane } from 'evergreen-ui';
+import { ProjectMap } from './components/ProjectMap';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [dataState, setDataState] = useState({
+        loaded: false,
+        data: null
+    })
+
+    if (!dataState.loaded) {
+        fetch("cycle_projects.json")
+            .then((r) => r.json())
+            .then((data) => setDataState({loaded: true, data}));
+    }
+
+    return (
+        <Pane width="100vw" height="100vh" position="absolute">
+            <ProjectMap loaded={dataState.loaded} data={dataState.data} />
+        </Pane>
+    );
 }
 
 export default App;
